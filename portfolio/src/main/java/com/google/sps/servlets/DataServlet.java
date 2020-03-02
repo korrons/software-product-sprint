fa09168e843de01b111b4c63ca6af645841f38ae
korrons <45104017+korrons@users.noreply.github.com>
2020-03-01 22:36:07 -0500

    Update DataServlet.java
    Added arraylist of hard coded messages/comments, convert strings to json

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import com.google.gson.Gson;
+import java.util.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +25,21 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
+List<String> messages;
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Korron!</h1>");
+     messages = new ArrayList<>();
+     messages.add("Hi Korron!");
+     messages.add("Nice portfolio!");
+     messages.add("Hello World!");
+    // Convert the messages to JSON
+    String json = convertToJson(messages);
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
+  private String convertToJson(Object object){
+       Gson gson = new Gson();
+       return gson.toJson(object);
 }
+ }
